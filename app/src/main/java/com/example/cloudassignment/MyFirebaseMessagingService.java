47ada52145326e9e3fae8150f9039a3048cf442c
Mohamed Samy <mamo.sha3ar@gmail.com>
2019-04-11 14:07:28 +0200

    second assignment

--- app/src/main/java/com/example/cloudassignment/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/cloudassignment/MyFirebaseMessagingService.java
@@ -3,15 +3,30 @@ package com.example.cloudassignment;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    DatabaseReference database;
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
         showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+
+        String notification = remoteMessage.getNotification().getBody();
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+        String str_date = sdf.format(date);
+        database = FirebaseDatabase.getInstance().getReference("Message").child(str_date);
+        database.setValue(notification);
     }
 
     public void showNotification(String title,String message){
