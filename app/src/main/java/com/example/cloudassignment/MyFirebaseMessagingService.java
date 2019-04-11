7dd2a7c96286bbe8bbd1938cc7ea669dcd8de22d
Mohamed Samy <mamo.sha3ar@gmail.com>
2019-04-11 13:58:49 +0200

    first assignment

---
+++ app/src/main/java/com/example/cloudassignment/MyFirebaseMessagingService.java
@@ -0,0 +1,29 @@
+package com.example.cloudassignment;
+
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+    }
+
+    public void showNotification(String title,String message){
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,"MyNotifications")
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setAutoCancel(true)
+                .setContentText(message);
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        manager.notify(999,builder.build());
+    }
+
+
+}
